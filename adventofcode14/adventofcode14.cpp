17addde739ad0614ff788c7a2fb2c0bbc03980ee
Trisslotten <tristan23@live.se>
2019-12-15 22:20:47 +0100

    working part 1

--- adventofcode14/adventofcode14.cpp
+++ adventofcode14/adventofcode14.cpp
@@ -8,7 +8,7 @@
 struct Requirement
 {
 	std::string element;
-	long num_required = 0;
+	long num_required = 1;
 };
 
 struct ReactionNode
@@ -18,27 +18,55 @@ struct ReactionNode
 };
 
 std::unordered_map<std::string, ReactionNode> reactions;
-std::unordered_map<std::string, long> temp_needed;
-std::unordered_map<std::string, long> spare;
-std::unordered_map<std::string, std::unordered_map<std::string, long>> total_needed;
+std::unordered_map<std::string, long> produced;
+std::unordered_map<std::string, long> extra;
+std::unordered_map<std::string, std::unordered_map<std::string, long>> total_produced;
 
-void setTotalRequirements(const std::string& element, long num_needed, long depth = 0)
+
+void setTotalRequirements(const std::string& element, long num_wanted, long depth = 0)
 {
-	auto reaction = reactions[element];
+	auto& reaction = reactions[element];
 
-	long mult = num_needed / reaction.num_from_reaction;
-	mult += (num_needed % reaction.num_from_reaction) > 0 ? 1 : 0;
-	spare[element] += num_needed % reaction.num_from_reaction;
-
-	for (auto& req : reaction.requirements)
+	long num_to_produce = 0; // = num_wanted - extra[element];
+	long extra_left = 0;
+	if (num_wanted < extra[element])
 	{
-		temp_needed[req.element] += mult * req.num_required;
+		num_to_produce = 0;
+		extra_left = extra[element] - num_wanted;
+	}
+	else
+	{
+		num_to_produce = num_wanted - extra[element];
+	}
+
+	if (extra[element] > 0)
+	{
 		for (int i = 0; i < depth; i++)
-		{
 			std::cout << "  ";
-		}
-		std::cout << req.element << ": " << mult * req.num_required << "\n";
-		setTotalRequirements(req.element, mult * req.num_required, depth + 1);
+		std::cout << "Using " << extra[element] << " spare " << element << "\n";
+	}
+	for (int i = 0; i < depth; i++)
+		std::cout << "  ";
+	std::cout << "Producing " << num_to_produce << " " << element << "\n";
+
+	long mult = num_to_produce / reaction.num_from_reaction;
+	mult += (num_to_produce % reaction.num_from_reaction) > 0 ? 1 : 0;
+	if ((num_to_produce % reaction.num_from_reaction) > 0)
+		extra[element] = reaction.num_from_reaction - (num_to_produce % reaction.num_from_reaction);
+	else
+		extra[element] = extra_left;
+
+	if (extra[element] > 0)
+	{
+		for (int i = 0; i < depth; i++)
+			std::cout << "  ";
+		std::cout << "Produced " << extra[element] << " spare " << element << "\n";
+	}
+
+	for (auto& requirement : reaction.requirements)
+	{
+		produced[requirement.element] += mult * requirement.num_required;
+		setTotalRequirements(requirement.element, mult * requirement.num_required, depth + 1);
 	}
 }
 
@@ -89,22 +117,18 @@ void main()
 	{
 		std::string element = reaction.first;
 
-		long mult = temp_needed[element] / reaction.second.num_from_reaction;
-		mult += (temp_needed[element] % reaction.second.num_from_reaction) > 0 ? 1 : 0;
+		long mult = produced[element] / reaction.second.num_from_reaction;
+		mult += (produced[element] % reaction.second.num_from_reaction) > 0 ? 1 : 0;
 
-		for (auto req : reaction.second.requirements)	
+		for (auto req : reaction.second.requirements)
 		{
-			total_needed[req.element][element] = mult * req.num_required;
+			total_produced[req.element][element] = mult * req.num_required;
 		}
 	}
-	
 
-	int sum = 0;
-	for (auto& i : total_needed["ORE"])
-	{
-		//std::cout << i.first << ": " << i.second << "\n";
-		sum += i.second;
-	}
+	long sum = 0;
+	for(auto p : total_produced["ORE"])
+		sum += p.second;
 
 	std::cout << sum << "\n";
 }
