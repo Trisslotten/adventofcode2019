6e242ca0c722edaa1e5818ce7b64d4d6343c4044
Trisslotten <tristan23@live.se>
2019-12-15 22:44:44 +0100

    day 14: part 2

--- adventofcode14/adventofcode14.cpp
+++ adventofcode14/adventofcode14.cpp
@@ -4,31 +4,33 @@
 #include <fstream>
 #include <sstream>
 #include <algorithm>
+#include "adventofcode14.h"
 
 struct Requirement
 {
 	std::string element;
-	long num_required = 1;
+	int64_t num_required = 1;
 };
 
 struct ReactionNode
 {
-	long num_from_reaction = 1;
+	int64_t num_from_reaction = 1;
 	std::vector<Requirement> requirements;
 };
 
 std::unordered_map<std::string, ReactionNode> reactions;
-std::unordered_map<std::string, long> produced;
-std::unordered_map<std::string, long> extra;
-std::unordered_map<std::string, std::unordered_map<std::string, long>> total_produced;
+
+std::unordered_map<std::string, int64_t> produced;
+std::unordered_map<std::string, int64_t> extra;
+std::unordered_map<std::string, std::unordered_map<std::string, int64_t>> total_produced;
 
 
-void setTotalRequirements(const std::string& element, long num_wanted, long depth = 0)
+void setTotalRequirements(const std::string& element, int64_t num_wanted, int64_t depth = 0)
 {
 	auto& reaction = reactions[element];
 
-	long num_to_produce = 0; // = num_wanted - extra[element];
-	long extra_left = 0;
+	int64_t num_to_produce = 0; // = num_wanted - extra[element];
+	int64_t extra_left = 0;
 	if (num_wanted < extra[element])
 	{
 		num_to_produce = 0;
@@ -38,7 +40,8 @@ void setTotalRequirements(const std::string& element, long num_wanted, long dept
 	{
 		num_to_produce = num_wanted - extra[element];
 	}
-
+	
+	/*
 	if (extra[element] > 0)
 	{
 		for (int i = 0; i < depth; i++)
@@ -48,26 +51,55 @@ void setTotalRequirements(const std::string& element, long num_wanted, long dept
 	for (int i = 0; i < depth; i++)
 		std::cout << "  ";
 	std::cout << "Producing " << num_to_produce << " " << element << "\n";
-
-	long mult = num_to_produce / reaction.num_from_reaction;
+	*/
+	
+	int64_t mult = num_to_produce / reaction.num_from_reaction;
 	mult += (num_to_produce % reaction.num_from_reaction) > 0 ? 1 : 0;
 	if ((num_to_produce % reaction.num_from_reaction) > 0)
 		extra[element] = reaction.num_from_reaction - (num_to_produce % reaction.num_from_reaction);
 	else
 		extra[element] = extra_left;
 
+	/*
 	if (extra[element] > 0)
 	{
 		for (int i = 0; i < depth; i++)
 			std::cout << "  ";
 		std::cout << "Produced " << extra[element] << " spare " << element << "\n";
 	}
+	*/
+	
+	
 
 	for (auto& requirement : reaction.requirements)
 	{
 		produced[requirement.element] += mult * requirement.num_required;
 		setTotalRequirements(requirement.element, mult * requirement.num_required, depth + 1);
 	}
+}
+
+void part1()
+{
+	setTotalRequirements("FUEL", 1);
+
+	for (auto& reaction : reactions)
+	{
+		std::string element = reaction.first;
+
+		int64_t mult = produced[element] / reaction.second.num_from_reaction;
+		mult += (produced[element] % reaction.second.num_from_reaction) > 0 ? 1 : 0;
+
+		for (auto req : reaction.second.requirements)
+		{
+			total_produced[req.element][element] = mult * req.num_required;
+		}
+	}
+
+	int64_t sum = 0;
+	for (auto p : total_produced["ORE"])
+		sum += p.second;
+
+	std::cout << sum << "\n";
 }
 
 void main()
@@ -88,7 +120,7 @@ void main()
 			while (req_str.size() > 0)
 			{
 				size_t next;
-				long num_required = std::stol(req_str, &next);
+				int64_t num_required = std::stol(req_str, &next);
 
 				std::string rest = req_str.substr(next + 1);
 				next = rest.find_first_of(',');
@@ -102,33 +134,55 @@ void main()
 			}
 
 			size_t next;
-			long num_from_reaction = std::stol(result_str, &next);
+			int64_t num_from_reaction = std::stol(result_str, &next);
 			result = result_str.substr(next + 1);
 
-			reactions[result] = { num_from_reaction,requirements };
+			reactions[result] = { num_from_reaction, requirements };
 
 			//std::cout << result << ": " << num_from_reaction << "\n";
 		}
 		data.close();
 	}
-	setTotalRequirements("FUEL", 1);
 
-	for (auto& reaction : reactions)
+	//part1();
+	int64_t start = 0;
+	int64_t i = 1;
+	bool running = true;
+	while (running)
 	{
-		std::string element = reaction.first;
+		int64_t n = start + i*i*i;
 
-		long mult = produced[element] / reaction.second.num_from_reaction;
-		mult += (produced[element] % reaction.second.num_from_reaction) > 0 ? 1 : 0;
+		setTotalRequirements("FUEL", n);
 
-		for (auto req : reaction.second.requirements)
+		for (auto& reaction : reactions)
 		{
-			total_produced[req.element][element] = mult * req.num_required;
+			std::string element = reaction.first;
+
+			int64_t mult = produced[element] / reaction.second.num_from_reaction;
+			mult += (produced[element] % reaction.second.num_from_reaction) > 0 ? 1 : 0;
+
+			for (auto req : reaction.second.requirements)
+			{
+				total_produced[req.element][element] = mult * req.num_required;
+			}
 		}
+		int64_t sum = 0;
+		for (auto p : total_produced["ORE"])
+			sum += p.second;
+		std::cout << n << ": " << sum << "\n";
+
+		if (sum > 1000000000000L)
+		{
+			int64_t old_start = start;
+			start = i-1;
+			start *= start * start;
+			start += old_start;
+			i = -1;
+		}
+
+		produced.clear();
+		extra.clear();
+		total_produced.clear();
+		i++;
 	}
-
-	long sum = 0;
-	for(auto p : total_produced["ORE"])
-		sum += p.second;
-
-	std::cout << sum << "\n";
 }
